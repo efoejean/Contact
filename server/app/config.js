cad35e87e46c3a8702d6cdda07843c5cb2411741
efoejean <77749280+efoejean@users.noreply.github.com>
2021-11-11 12:51:03 -0600

    Set up the configuration to connect to the database

--- server/app/config.js
+++ server/app/config.js
@@ -1,13 +1,14 @@
 // Provides config for the app in conjunction with dotenv
 // Config is the only place accesses 'dotenv'
 import dotenv from "dotenv";
-
 dotenv.config();
 
 export default {
+  db: {
+    clientURL: process.env.DB_CLIENT_URL,
+    name: "Contact",
+    collection: "Contact",
+  },
+
   port: process.env.PORT || 3000,
-  db:
-    process.env.DB_CLIENT_URL ||
-    // Use localhost if no DB_CLIENT_URL is provided
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
 };
