955c83d4df8bd9ae03fada00bb3f5bf7546956d2
efoejean <77749280+efoejean@users.noreply.github.com>
2021-11-11 13:13:10 -0600

    Set the route to access the Api

--- server/app/router.js
+++ server/app/router.js
@@ -4,4 +4,8 @@ const router = new Router();
 
 // TODO: Add routes here (maybe 🤔 start with a GET test route)
 
+router.get("/", (_,res) =>{
+    res.send("hello from the Api")
+})
+
 export default router;
